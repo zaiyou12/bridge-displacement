9b6c4e5fa881174667d8b8bfbaf1ab837f09c98e
Aaron So <jae.woo@blackrubystudio.com>
2017-12-25 20:29:35 +0900

    한글 주석 추가

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ from skimage.feature import canny
 from skimage.color import rgb2gray
 
 
+# 환경설정
 def initial_setting():
     if os.path.exists('setting.env'):
         print('Importing environment from setting.env...')
@@ -20,6 +21,7 @@ def initial_setting():
                 # print(var[0], '=', var[1])
 
 
+# 최근 파일 불러오기
 def get_latest_file(file_url):
     list_of_files = glob.glob(file_url)
     if not list_of_files:
@@ -30,8 +32,10 @@ def get_latest_file(file_url):
         return latest_file
 
 
+# 이미지 내 원 인식
 def find_circle(image, x_start, y_start, mask=False):
     # pre-processing
+    # 사전 작업
     image = rgb2gray(image)
     if mask:
         mask = image > 0.1
@@ -41,6 +45,10 @@ def find_circle(image, x_start, y_start, mask=False):
     # The accuracy corresponds to the bin size of a major axis.
     # The value is chosen in order to get a single high accumulator.
     # The threshold eliminates low accumulators
+    # 허프 변환 수행
+    # 정확도는 장축의 크기에 따라 달라집니다
+    # 하나의 높은 값을 얻기 위한 계산이며
+    # 임계값은 낮은 정확도의 값을 제거 합니다.
     result = hough_ellipse(image)
     result.sort(order='accumulator')
 
@@ -50,6 +58,7 @@ def find_circle(image, x_start, y_start, mask=False):
     return yc+y_start, xc+x_start
 
 
+# 거리 계산하기
 def get_distance():
     # update all variable
     initial_setting()
@@ -69,19 +78,23 @@ def get_distance():
     y3_end = int(os.environ.get('y3_end'))
 
     # check all variables before get distance from image
+    # 모든 변수가 있는지 확인
     if any(v is None for v in [file_url, reference_distance, x1_start, x1_end, y1_start, y1_end, x2_start,
                                x2_end, y2_start, y2_end, x3_start, x3_end, y3_start, y3_end]):
         print('Some variable is none, please check setting.env')
     else:
         # get images and pre-processing
+        # 이미지를 불러온뒤 전처리
         image_1 = io.imread(file_url)[x1_start:x1_end, y1_start:y1_end]
         image_2 = io.imread(file_url)[x2_start:x2_end, y2_start:y2_end]
         image_3 = io.imread(file_url)[x3_start:x3_end, y3_start:y3_end]
         # find circles and get center of the circles
+        # 원과 원의 중심 찾기
         yc1, xc1 = find_circle(image_1, x_start=x1_start, y_start=y1_start)
         yc2, xc2 = find_circle(image_2, x_start=x2_start, y_start=y2_start)
         yc3, xc3 = find_circle(image_3, x_start=x3_start, y_start=y3_start, mask=True)
         # calculate distance
+        # 거리 계산
         v1 = np.array([yc2, xc2]) - np.array([yc1, xc1])  # vector form point 1 to point 2
         v2 = np.array([yc3, xc3]) - np.array([yc1, xc1])  # vector form point 1 to point 3
         delta = np.vdot(v1, v2) / (np.linalg.norm(v1))  # get distance between point 1 and point 2
